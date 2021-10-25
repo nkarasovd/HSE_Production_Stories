f01a178211dff8b314eebcb673a98286c05f4b23
Nikolai <nikolai.karasoff@yandex.ru>
2021-10-25 12:24:22 +0300

    Add environment code

---
+++ Task_5/mapgen/env.py
@@ -0,0 +1,149 @@
+import math
+import random
+
+import gym
+import numpy as np
+from PIL import Image
+from gym import spaces
+
+from mapgen.agent import Agent, Position, Orientation
+from mapgen.dungeon import DungeonGenerator
+from mapgen.map import Map, Move
+
+
+class Dungeon(gym.Env):
+    def __init__(self,
+                 width=64,
+                 height=64,
+                 max_rooms=25,
+                 min_room_xy=10,
+                 max_room_xy=25,
+                 observation_size: int = 11,
+                 vision_radius: int = 5,
+                 max_steps: int = 2000
+                 ):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.max_rooms = max_rooms
+        self.min_room_xy = min_room_xy
+        self.max_room_xy = max_room_xy
+
+        # observation parameters
+        self.observation_size = observation_size
+        self.vision_radius = vision_radius
+
+        # map generator
+        self._gen = DungeonGenerator(width, height, max_rooms, min_room_xy, max_room_xy)
+        self._map: Map = None
+        self._explored_area = None
+        self._agent_pos = None
+        self._step = 0
+        self._max_steps = max_steps
+
+        self.observation_space = spaces.Box(0, 1, [observation_size, observation_size, 4])
+        self.action_space = spaces.Discrete(3)
+
+    def reset(self):
+        """Resets the environment to an initial state and returns an initial
+        observation.
+
+        Note that this function should not reset the environment's random
+        number generator(s); random variables in the environment's state should
+        be sampled independently between multiple calls to `reset()`. In other
+        words, each call of `reset()` should yield an environment suitable for
+        a new episode, independent of previous episodes.
+
+        Returns:
+            observation (object): the initial observation.
+        """
+        # generate level
+        self._gen.gen_level()
+        self._map = Map(self._gen.level)
+
+        # place agent
+        x, y = self._map.get_random_free_position()
+        self._agent = Agent(Position(x, y), Orientation(np.random.randint(4)), vision_radius=self.vision_radius,
+                            vision_angle=math.pi / 2)
+        _ = self._map.update_explored_area(self._agent, align_with_map=True)
+        self._step = 1
+
+        observation = self._map.get_observation(self._agent, self.observation_size)
+        return observation
+
+    def step(self, action: int):
+        """Run one timestep of the environment's dynamics. When end of
+        episode is reached, you are responsible for calling `reset()`
+        to reset this environment's state.
+
+        Accepts an action and returns a tuple (observation, reward, done, info).
+
+        Args:
+            action (object): an action provided by the agent
+
+        Returns:
+            observation (object): agent's observation of the current environment
+            reward (float) : amount of reward returned after previous action
+            done (bool): whether the episode has ended, in which case further step() calls will return undefined results
+            info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
+              - step: current step number
+              - total_cells: total number of visible cells for current map
+              - total_explored: total number of explored cells (map is solved when total_explored == total_cells)
+              - new_explored: number of explored cells during this step
+              - moved: whether an agent made a move (didn't collide with an obstacle)
+        """
+        action = Move(action - 1)
+        observation, explored, done, moved, is_new = self._map.step(self._agent, action, self.observation_size)
+
+        # set reward as a fraction of new explored cells (so total reward is 1.0)
+        reward = explored / self._map._visible_cells
+
+        info = {
+            "step": self._step,
+            "total_cells": self._map._visible_cells,
+            "total_explored": self._map._total_explored,
+            "new_explored": explored,
+            "avg_explored_per_step": self._map._total_explored / self._step,
+            "moved": moved,
+            "is_new": is_new
+        }
+        self._step += 1
+
+        return observation, reward, done or self._step == self._max_steps, info
+
+    def render(self, mode='rgb_array', size=None):
+        """Renders the environment.
+
+        The set of supported modes varies per environment. (And some
+        environments do not support rendering at all.) By convention,
+        if mode is:
+
+        - human: render to the current display or terminal and
+          return nothing. Usually for human consumption.
+        - rgb_array: Return an numpy.ndarray with shape (x, y, 3),
+          representing RGB values for an x-by-y pixel image, suitable
+          for turning into a video.
+
+        Args:
+            mode (str): the mode to render with
+
+        """
+        if mode == "rgb_array":
+            render_img = Image.fromarray(self._map.render(self._agent))
+
+            if size is not None:
+                render_img = render_img.resize((size, size), Image.NEAREST)
+
+            return np.asarray(render_img)
+        elif mode == "human":
+            print(f"Step {self._step}")
+            print(self._map.show(self._agent))
+            print(
+                f"Explored: {self._map._total_explored}/{self._map._visible_cells} cells ({self._map._total_explored / self._map._visible_cells * 100:.2f}%)")
+        else:
+            raise RuntimeError(f"Unknown render mode, expected one of ['human', 'rgb_array']. Got: {mode}")
+
+    def seed(self, seed=None):
+        random.seed(seed)
+        np.random.seed(seed)
+        self.action_space.seed(seed)
---
+++ Task_5/mapgen/map.py
@@ -0,0 +1,220 @@
+from copy import deepcopy
+from dataclasses import dataclass
+from typing import List, Tuple
+
+import numpy as np
+
+from mapgen.agent import Agent, Move, Orientation
+from mapgen.dungeon import TileKind
+
+TILES = {
+    TileKind.OCCUPIED: "█",
+    TileKind.FREE: ".",
+    TileKind.UNKNOWN: "×",
+    TileKind.AGENT: "O",
+    TileKind.EXPLORED: "_"
+}
+
+TILES_COLORS = {
+    TileKind.OCCUPIED: "#674d3c",
+    TileKind.FREE: "#d9ad7c",
+    TileKind.UNKNOWN: "#a2836e",
+    TileKind.AGENT: "#c83349",
+    TileKind.EXPLORED: "#fff2df"
+}
+
+
+@dataclass
+class Slice:
+    x_min: int
+    x_max: int
+    y_min: int
+    y_max: int
+
+    def __iter__(self):
+        return iter((self.x_min, self.x_max, self.y_min, self.y_max))
+
+
+class Map:
+    def __init__(
+            self,
+            tiles: List[List[TileKind]],
+            explored_area: List[List[bool]] = None,
+            trajectory: List[List[bool]] = None
+    ):
+        self.tiles = np.array(tiles).astype(int)
+        self._explored_area = np.array(explored_area) if explored_area is not None else np.zeros_like(self.tiles,
+                                                                                                      dtype=bool)
+        self._trajectory = np.array(trajectory) if trajectory is not None else np.zeros_like(self.tiles, dtype=bool)
+
+        self._render = '\n'.join([''.join([TILES[TileKind(tile)] for tile in row]) for row in tiles])
+
+        self._visible_cells = np.sum(np.where(self.tiles != TileKind.UNKNOWN, True, False))
+        self._total_explored = 0
+
+    @property
+    def size(self):
+        return self.tiles.shape
+
+    def __str__(self):
+        return self._render
+
+    def show(self, agent: Agent = None):
+        tiles = self.tiles.copy()
+        tiles = np.where(self._explored_area, tiles, TileKind.UNKNOWN)
+        if agent:
+            tiles[agent.position.y, agent.position.x] = TileKind.AGENT
+        render = '\n'.join([''.join([TILES[TileKind(tile)] for tile in row]) for row in tiles])
+        return render
+
+    def render(self, agent: Agent):
+        frame = np.array([[self.hex2rgb(TILES_COLORS[tile]) for tile in row] for row in self.tiles])
+        frame = 0.7 * frame + 0.3 * np.tile((1 - self._explored_area * (self.tiles != TileKind.UNKNOWN)) * 200,
+                                            (3, 1, 1)).transpose((1, 2, 0))
+
+        # plot trajectory
+        for y, x in zip(*np.nonzero(self._trajectory)):
+            frame[y, x] = self.hex2rgb(TILES_COLORS[TileKind.EXPLORED])
+
+        frame[agent.position.y, agent.position.x] = self.hex2rgb(TILES_COLORS[TileKind.AGENT])
+
+        return frame.astype(np.uint8)
+
+    @staticmethod
+    def hex2rgb(hex: str) -> Tuple[int]:
+        h = hex.lstrip("#")
+        return tuple(int(h[i:i + 2], 16) for i in (0, 2, 4))
+
+    @property
+    def explored_area(self):
+        return '\n'.join([''.join(row) for row in np.where(self._explored_area, ".", "×")])
+
+    def step(self, agent: Agent, move: Move, observation_size: int):
+        position = deepcopy(agent.position)
+        moved = False
+        if move == Move.FORWARD:
+            if agent.orientation == Orientation.EAST:
+                position.x += 1
+            elif agent.orientation == Orientation.NORTH:
+                position.y -= 1
+            elif agent.orientation == Orientation.WEST:
+                position.x -= 1
+            elif agent.orientation == Orientation.SOUTH:
+                position.y += 1
+
+            # check validity
+            try:
+                if self.tiles[position.y, position.x] == TileKind.FREE:
+                    agent.position = position
+                    moved = True
+                else:
+                    moved = False
+            except IndexError as err:
+                moved = False
+
+        else:
+            orientation = agent.orientation + move
+            if orientation > Orientation.SOUTH:
+                orientation = Orientation.EAST
+            elif orientation < Orientation.EAST:
+                orientation = Orientation.SOUTH
+            agent.orientation = orientation
+            moved = True
+
+        is_new = int(not self._trajectory[agent.position.y, agent.position.x])
+
+        explored = self.update_explored_area(agent, align_with_map=True)
+        success = self._total_explored == self._visible_cells
+        obs = self.get_observation(agent, observation_size)
+        return obs, explored, success, moved, is_new
+
+    def get_random_free_position(self) -> Tuple[int, int]:
+
+        ys, xs = np.nonzero(np.where(self.tiles == TileKind.FREE, True, False))
+        idx = np.random.randint(len(xs))
+        return xs[idx], ys[idx]
+
+    def get_map_slice_coords(self, agent: Agent, slice_size: int) -> Tuple[Slice, Slice]:
+        x0, y0 = agent.position
+
+        w = h = slice_size
+        x = x0 - (slice_size - 1) // 2
+        y = y0 - (slice_size - 1) // 2
+
+        x_max, y_max = self.size
+
+        x_min, x_max = np.clip([x, x + h], a_min=0, a_max=self.size[1])
+        y_min, y_max = np.clip([y, y + w], a_min=0, a_max=self.size[0])
+
+        map_slice = Slice(x_min, x_max, y_min, y_max)
+        observation_slice = Slice(max(-x, 0), max(-x, 0) + x_max - x_min, max(-y, 0), max(-y, 0) + y_max - y_min)
+
+        return map_slice, observation_slice
+
+    def update_explored_area(self, agent: Agent, align_with_map: bool = False) -> int:
+
+        self._trajectory[agent.position.y, agent.position.x] = 1
+
+        map_slice, obs_slice = self.get_map_slice_coords(agent, agent.visible_area.shape[0])
+
+        # get observation_slice
+        x_min, x_max, y_min, y_max = obs_slice
+        vis_area_slice = agent.visible_area[y_min: y_max, x_min: x_max]
+
+        # add new observation
+        x_min, x_max, y_min, y_max = map_slice
+
+        old_explored = np.sum(self._explored_area)
+        self._explored_area[y_min: y_max, x_min: x_max] += vis_area_slice.astype(bool)
+
+        # align with map
+        if align_with_map:
+            self._explored_area *= np.where(self.tiles != TileKind.UNKNOWN, True, False)
+
+        new_explored = np.sum(self._explored_area)
+
+        self._total_explored = new_explored
+        return new_explored - old_explored
+
+    def get_observation(self, agent, observation_size: int):
+        observation = np.zeros((3, observation_size, observation_size), dtype=int)
+
+        map_slice, obs_slice = self.get_map_slice_coords(agent, observation_size)
+
+        # get map slices
+        x_min, x_max, y_min, y_max = map_slice
+        map_slice = self.tiles[y_min: y_max, x_min: x_max]
+        explored_slice = self._explored_area[y_min: y_max, x_min: x_max]
+        visited_slice = self._trajectory[y_min: y_max, x_min: x_max]
+
+        # set by observation slice
+        x_min, x_max, y_min, y_max = obs_slice
+
+        observation[0, y_min: y_max, x_min: x_max] = map_slice
+        observation[1, y_min: y_max, x_min: x_max] = explored_slice
+        observation[2, y_min: y_max, x_min: x_max] = visited_slice
+
+        # rotate
+        observation[0] = np.rot90(observation[0], k=-agent.orientation + 1)
+        observation[1] = np.rot90(observation[1], k=-agent.orientation + 1)
+        observation[2] = np.rot90(observation[2], k=-agent.orientation + 1)
+
+        x = observation[0] * (1 - observation[1])
+        x = (np.arange(3) == x[..., None]).astype(int).astype(np.float32)
+
+        agent_position = np.zeros_like(observation[2])
+        midpoint = agent_position.shape[0] // 2
+        agent_position[midpoint, midpoint] = 1
+        x = np.concatenate([
+            x,
+            np.expand_dims(observation[2], axis=-1),
+        ], axis=-1).astype(np.float32)
+
+        return x
+
+    @staticmethod
+    def map_to_numpy(obs):
+        x = obs.tiles * (1 - obs._explored_area)
+        # one-hot
+        x = (np.arange(x.max()) == x[..., None] - 1).astype(int)
+        return x
